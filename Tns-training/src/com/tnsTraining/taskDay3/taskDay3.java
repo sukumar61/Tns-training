aa9099df73e3a3c9dc99da3d8c46be4c0e734c3b
SukumarRenukuntla <sukumarrenukuntla@gmail.com>
2026-05-26 15:02:13 +0530

    Added  package com.tnsTranining.taskDay3 and class taskDay3.java

---
+++ Tns-training/src/com/tnsTraining/taskDay3/taskDay3.java
@@ -0,0 +1,12 @@
+package com.tnsTraining.taskDay3;
+
+public class taskDay3 {
+
+	public static void main(String[] args) {
+		String arr[]={"a","i","s","h","u"};
+		for (int i=arr.length-1;i>=0;i--) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
---
+++ Tns-training/src/first_package/myclass.java
@@ -0,0 +1,10 @@
+package first_package;
+
+public class myclass {
+
+	public static void main(String[] args) {
+		System.out.println(false);
+
+	}
+
+}
